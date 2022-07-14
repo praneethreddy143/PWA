2d75180ef4132701ecd5337e9b9c3f11b6ade8d2
praneethreddy143 <99461286+praneethreddy143@users.noreply.github.com>
2022-07-14 15:20:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener("install" , e=>{
-   console.log("install");
+   e.waitUntil(
+    caches.open("static").then(cache =>{
+       return cache.addAll(["./","./src/master.css","./images/logo192.png"]);
+    })
+   );
 });
